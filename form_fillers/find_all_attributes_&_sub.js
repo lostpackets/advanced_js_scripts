0d35d9096e9fed65111dc40e0622ad261f98a6e8
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-02-05 14:48:06 +0000

    Update find_all_attributes_&_sub.js

--- form_fillers/find_all_attributes_&_sub.js
+++ form_fillers/find_all_attributes_&_sub.js
@@ -1,4 +1,6 @@
-let elements = document.querySelectorAll('[value=""]');
-for (let i = 0; i < elements.length; i++) {
+(function() {
+  let elements = document.querySelectorAll('[value=""]');
+  for (let i = 0; i < elements.length; i++) {
     elements[i].value = (i % 2 === 0) ? "0" : "1";
-}
+  }
+})();
