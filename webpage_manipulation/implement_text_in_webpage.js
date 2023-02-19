e0015a27b9eee69aeace60fc3e8e2333c7088d0c
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-02-19 21:56:33 +0000

    Create implement_text_in_webpage.js

---
+++ webpage_manipulation/implement_text_in_webpage.js
@@ -0,0 +1,12 @@
+// Create a new textbox element
+const textBox = document.createElement('input');
+
+// Set the textbox's type and value properties
+textBox.type = 'text';
+textBox.value = 'Hello World';
+
+// Set the textbox's style properties to make it red
+textBox.style.color = 'red';
+
+// Append the textbox to the body of the HTML document
+document.body.appendChild(textBox);
